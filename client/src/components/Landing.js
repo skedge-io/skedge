8ad400520a3c27086d398a08a0374dc19b5585a4
Joerez <rezenbiz@gmail.com>
2018-07-09 18:46:51 -0700

    basic frontend

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Skedge
+      </h1>
+      Schedule Appointments, send reminders, gain repeat business, and increase your five star reviews!
+    </div>
+  )
+}
+
+export default Landing;
