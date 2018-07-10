9215d83e7d9ec6779788e031241643ec07ca42db
JamieRez <jamierezendes@gmail.com>
2018-07-09 18:42:54 -0700

    heroku init

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = '5000' || process.env.PORT;
+const port = process.env.PORT || '5000';
 
 const mongoose = require('mongoose');
 
