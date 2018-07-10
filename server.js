9238effbec478d9a80e2ed6df639366ab7797474
JamieRez <jamierezendes@gmail.com>
2018-07-09 18:29:26 -0700

    Heroku init

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('expres');
 const app = express();
+const port = '5000' || process.env.PORT;
 
 const mongoose = require('mongoose');
 
 app.get('/', (req,res) => {
   res.send('Hello Heroku');
+})
+
+app.listen(port, () => {
+  console.log("Skedge time");
 })
