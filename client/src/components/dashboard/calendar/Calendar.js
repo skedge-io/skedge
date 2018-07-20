685778a149ef70ac450251f8c1239802f41d612f
Joerez <rezenbiz@gmail.com>
2018-07-19 22:23:23 -0700

    added a space

--- client/src/components/dashboard/calendar/Calendar.js
+++ client/src/components/dashboard/calendar/Calendar.js
@@ -114,7 +114,7 @@ class DashCalendar extends Component {
                       <span className="card-title">{this.state.eventView.title}</span>
                       <p><b>Start</b>: {this.state.eventView.start} <b><br />End</b>: {this.state.eventView.end}</p>
                       <p><b>Phone</b>: {this.state.eventView.phone}</p>
-                      <p><b>Notes</b>:{this.state.eventView.desc}</p>
+                      <p><b>Notes</b>: {this.state.eventView.desc}</p>
                     </div>
                     <div className="card-action">
                       <a className="btn-floating waves-effect waves-light blue margin-right"><i className="material-icons">edit</i></a>
