3dbe715be106253e52a56e69eb5c00df061fee4c
Joerez <rezenbiz@gmail.com>
2018-07-23 00:14:40 -0700

    form connected to dashboard

--- client/src/components/appointments/AppointmentField.js
+++ client/src/components/appointments/AppointmentField.js
@@ -4,10 +4,11 @@ import React from 'react';
 import { Input } from 'react-materialize'
 
                   //same as props.input
-export default ({ onChange, defaultValue, input, type, label, meta: { error, touched } }) => {
+export default ({ onChange, name, defaultValue, input, type, label, meta: { error, touched } }) => {
   return (
     <div>
-      <Input placeHolder={label} defaultValue={defaultValue} onChange={onChange} type={type} {...input} style={{ marginBottom: '0px', height: '1rem' }} />
+      <label for={label}>{label}</label>
+      <Input placeholder={label} defaultValue={defaultValue} id={name} onChange={onChange} type={type} {...input} style={{ marginBottom: '0px', height: '1rem' }} />
       <div className="red-text" style={{ marginBottom: '0px' }}>
         {touched && error}
       </div>
