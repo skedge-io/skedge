b380636a7aa5fa322a2146bb9360ebee98fb0b5c
Joerez <rezenbiz@gmail.com>
2018-08-18 11:42:57 -0700

    account register works

---
+++ client/src/components/account/owner/formFields.js
@@ -0,0 +1,7 @@
+//change this file to change the number of inputs, or edit them.
+export default [
+  { label: 'First and Last Name', type: 'text', name: 'name',  noValueError: 'You must provide a first and last name.' },
+  { label: 'Business Name', type: 'text', name: 'business',  noValueError: 'You must provide a business name.'  },
+  { label: 'Cell Phone Number', type: 'text', name: 'phone',  noValueError: 'You must provide a phone number.'  },
+  { label: '', type: 'hidden', name: 'admin',  checked: 'checked' }
+];
