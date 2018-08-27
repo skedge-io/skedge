69767c0e6d9d5649295dce4c0a258ee097c90985
Joerez <rezenbiz@gmail.com>
2018-08-27 12:13:45 -0700

    polished the tetrahedrons

--- client/src/components/dashboard/MobileMenu.js
+++ client/src/components/dashboard/MobileMenu.js
@@ -24,11 +24,11 @@ class MobileMenu extends Component {
 
         <div className="mob-menu" className={this.state.mobActive ? 'mob-menu on': 'mob-menu'}>
           <div className={this.state.mobActive ? 'mobileMenuLinks on': 'mobileMenuLinks'}>
-            <Link  id={this.props.wambo} to="/dashboard">Calendar</Link>
-            <Link  id={this.props.index1} to="/dashboard/employees">Employees</Link>
-            <Link  id={this.props.index2} to="/dashboard/statistics">Statistics</Link>
-            <Link  id={this.props.index3} to="/dashboard/campaigns">Campaigns</Link>
-            <Link  id={this.props.index4} to="/dashboard/contacts">Manage Contacts</Link>
+            <Link onClick={this.toggleClass.bind(this)}  id={this.props.wambo} to="/dashboard">Calendar</Link>
+            <Link onClick={this.toggleClass.bind(this)}  id={this.props.index1} to="/dashboard/employees">Employees</Link>
+            <Link onClick={this.toggleClass.bind(this)}  id={this.props.index2} to="/dashboard/statistics">Statistics</Link>
+            <Link onClick={this.toggleClass.bind(this)}  id={this.props.index3} to="/dashboard/campaigns">Campaigns</Link>
+            <Link onClick={this.toggleClass.bind(this)}  id={this.props.index4} to="/dashboard/contacts">Manage Contacts</Link>
           </div>
         </div>
     </div>
