688a8e0ed8b4db061aa7d8660985f985687fafc6
Joerez <rezenbiz@gmail.com>
2018-09-01 18:17:38 -0700

    edit fields and new appointment fields are now in the same order

--- client/src/components/appointments/formFields.js
+++ client/src/components/appointments/formFields.js
@@ -1,10 +1,10 @@
 //change this file to change the number of inputs, or edit them.
 export default [
   { label: 'Client Name', type: 'text', name: 'customer', noValueError: 'You must provide a customer name.' },
+  { label: 'Employee Name', type: 'text', name: 'employee', noValueError: 'You must provide an employee' },
   { label: 'Client Phone Number', type: 'text', name: 'phone', noValueError: 'You must provide a phone number.' },
   { label: 'Date', name: 'date',  type: "date", onChange: function(e, value) {}, noValueError: 'You must provide a date' },
   { label: 'Start Time', name: 'startTime',  type: "time", onChange: function(e, value) {}, noValueError: 'You must provide a time' },
   { label: 'End Time', name: 'endTime',  type: "time", onChange: function(e, value) {}, noValueError: 'You must provide a time' },
-  { label: 'Employee Name', type: 'text', name: 'employee', noValueError: 'You must provide an employee' },
   { label: 'Additional Notes', type: 'text', name: 'desc' }
 ];
