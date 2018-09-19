12d53fe3a7bb63e9ba1504df826e908250f0cfa1
Joerez <rezenbiz@gmail.com>
2018-09-19 12:55:40 -0700

    changed customers to clients

--- client/src/components/dashboard/contacts/ContactList.js
+++ client/src/components/dashboard/contacts/ContactList.js
@@ -1,30 +1,128 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
 
+let dummyData = [
+  {
+    name : "John",
+    number : "123-456-7890",
+    lastVisit : "9/3/2018"
+  },
+  {
+    name : "John",
+    number : "123-456-7890",
+    lastVisit : "9/3/2018"
+  },
+  {
+    name : "John",
+    number : "123-456-7890",
+    lastVisit : "9/3/2018"
+  },
+  {
+    name : "John",
+    number : "123-456-7890",
+    lastVisit : "9/3/2018"
+  },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    },
+    {
+      name : "John",
+      number : "123-456-7890",
+      lastVisit : "9/3/2018"
+    }
+
+
+]
+
+
 class ContactList extends Component {
   constructor(props) {
     super(props)
 
-
     this.showContactDetails  = this.showContactDetails.bind(this);
+
 
     this.state = {
       showContact: false
     };
-
-
   }
 
 
-  showContactDetails() {
-    this.setState({ showContact: true })
+  showContactDetails(event) {
+    console.log(event.target);
+    this.setState({ showContact: true, details: {
+      name: event.target.name,
+      number: this.number,
+      lastVisit: this.lastVisit
+    } })
   }
-
 
   renderContactDetails() {
         return (
           <div className="contact-details">
-            <p>Name: John</p>
+            <p>Name: {this.state.details.name}</p>
             <p>Number: 123-456-7890</p>
             <p>Notes: </p>
             <p>Next Appointment: Unscheduled</p>
@@ -41,37 +139,10 @@ class ContactList extends Component {
     return (
       <div className="contacts-container">
         <div className="contact-list-container">
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
-          <Contact clicky={this.showContactDetails} name="John" number="123-456-7890" lastVisit="9/3/2018" />
+
+          {dummyData.map((data, index) => (
+            <Contact key={data.number} clicky={this.showContactDetails} name={data.name} number={data.number} lastVisit={data.lastVisit} />
+          ))}
 
         </div>
 
