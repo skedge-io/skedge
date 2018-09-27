72e88d7be7aa55df5d2ca19215f86d0a515e659c
Joerez <rezenbiz@gmail.com>
2018-09-26 21:40:12 -0700

    grab one client

--- client/src/components/dashboard/employees/EmployeeListItem.js
+++ client/src/components/dashboard/employees/EmployeeListItem.js
@@ -8,7 +8,7 @@ class EmployeeListItem extends Component {
       <div className="employee-list-item">
         <div className="list-item-info name">{this.props.name}</div>
         <div className="list-item-info">{this.props.appointments}</div>
-        <div className="list-item-info calander"><a href="#">Calander</a></div>
+        <div className="list-item-info calander"><a href="/calander/employeeId">Calander</a></div>
         <div className="list-item-info info"><span className="clickable">Info</span></div>
         <div className="list-item-info edit"><span className="clickable">Edit</span></div>
         <div className="list-item-info delete"><span className="clickable">Delete</span></div>
