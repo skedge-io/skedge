6c96ad9b5f9865e30500415f1c364697e2937970
Joerez <rezenbiz@gmail.com>
2018-12-22 11:00:39 -0800

    added service worker

--- client/src/index.js
+++ client/src/index.js
@@ -11,9 +11,18 @@ import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
+import * as serviceWorker from './registerServiceWorker';
+
+
 ReactDOM.render(
 <Provider store={store}>
   <App />
 </Provider>,
 document.querySelector('#root')
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
