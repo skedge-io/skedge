c1bdaa658812e389d2d5c3135219a93a8f671e48
Joerez <rezenbiz@gmail.com>
2019-01-02 00:14:22 -0800

    changed copy

--- client/src/components/dashboard/settings/JoinBox.js
+++ client/src/components/dashboard/settings/JoinBox.js
@@ -29,7 +29,7 @@ class JoinBox extends Component {
       <div className="settings-box">
         <div className="join-business-box">
           <h3>Join New Business</h3>
-          <p>Ask your manager to send you the ID of the business you are attempting to join.</p>
+          <p>Ask your manager to send you the code of the business you are attempting to join.</p>
           <input onChange={this.handleChange} className="copy-input-imp" value={this.state.value} />
           <button onClick={() => this.updateBusiness()} className="copy-button-imp">{this.state.success ? <span>Success</span> : <span>Join</span>}</button>
           <p>{this.state.success ? <span style={{color: 'green'}}>You have joined this business.</span> : <span></span>}</p>
