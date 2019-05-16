7d28b9ea9cd111801996b613d4487bd5cfe82185
Joerez <rezenbiz@gmail.com>
2019-05-15 17:51:41 -0700

    middleware for stripe checkout

--- controllers/billing.js
+++ controllers/billing.js
@@ -9,25 +9,18 @@ module.exports = app => {
   //make a payment
   app.post('/api/stripe', requireLogin, (req, res) => {
 
-  console.log('req', req);
-  console.log('id', req.body.id)
-  console.log('req.body', req.body)
-
   stripe.charges.create({
       amount: 1995,
       currency: 'usd',
       description: '$19.95 for upgraded plan',
       source: req.body.id
     }).then(() => {
-      console.log('we made it')
       User.findById(req.user.id).then((user) => {
         req.user.plan = 'Premium';
         req.user.save();
         res.send(user);
       })
     })
-
-
-
   });
+  
 };
